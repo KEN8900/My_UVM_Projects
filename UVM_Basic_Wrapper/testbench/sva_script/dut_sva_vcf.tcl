88ce84d79f98bbab65d15fa23a33d839f72fd00f
KEN8900 <kenhky@outlook.com>
2025-02-10 19:23:28 -0500

    Update Makefile, .tcl files, and sva_results from VC Formal.

---
+++ UVM_Basic_Wrapper/testbench/sva_script/dut_sva_vcf.tcl
@@ -0,0 +1,15 @@
+set_fml_appmode FPV
+set design dut
+
+set_app_var fml_mode_on true
+read_file -top $design -format sverilog -sva \
+          -vcs {-full64 -sverilog -f filelist.f}
+#read_waiver_file -elfiles aep.el
+
+# Creating clock and reset signals
+create_clock clk -period 100 
+create_reset rst_n -sense low
+
+# Runing a reset simulation
+sim_run -stable 
+sim_save_reset
